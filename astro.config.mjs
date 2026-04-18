880ad1bafb1bf57bb7ce11eb22534a18ee6b3ed3
weizu <mikeneko.kmy@gmail.com>
2026-04-18 20:45:53 +0900

    #A デザインのベース変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,33 +4,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://weizuSANN.github.io',
-	base: '/KisaragiBlog',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: 'https://weizuSANN.github.io',
+  base: '/KisaragiBlog',
+  integrations: [mdx(), sitemap()],
+
+  fonts: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
