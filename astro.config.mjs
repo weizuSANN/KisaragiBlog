d518a1dc229c02fc8aa8fdd95c67bdd5df4a6fa0
weizu <mikeneko.kmy@gmail.com>
2026-04-18 20:04:48 +0900

    FirstCommit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://weizuSANN.github.io',
+	base: '/KisaragiBlog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
